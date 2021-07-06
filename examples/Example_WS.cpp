31531ad12edfe88d485c5cdaaffbd8c88e69ed9c
Bowen Zhang <bowen.zhang@cern.ch>
2021-07-06 09:19:22 +0800

    more tests with Workspace

--- examples/Example_WS.cpp
+++ examples/Example_WS.cpp
@@ -8,12 +8,23 @@ void test_ws_info(const std::string& filename) {
     info->path = filename;
     info->workspace_name = "combined";
     info->use_asimov = false;
+    info->use_oneline_fit = false;
 
     WorkSpace* wst = new WorkSpace(info);
     wst->Check();
     wst->FitAll();
-    wst->CheckParaMaps();
+    // wst->CheckParaMaps();
+
+    info->tolerance = 1e-8;
+
+    WorkSpace* wst_up = new WorkSpace(info);
+    wst_up->FitWithAllParaFixed(wst->GetFittedNPs(), 1.0f);
+
+    WorkSpace* wst_down = new WorkSpace(info);
+    wst_down->FitWithAllParaFixed(wst->GetFittedNPs(), -1.0f);
 
     delete info;
     delete wst;
+    delete wst_up;
+    delete wst_down;
 }
