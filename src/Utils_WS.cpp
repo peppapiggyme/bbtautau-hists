b1586d7bf3356bcefee1e29590cd36e54e5008e7
Bowen Zhang <bowen.zhang@cern.ch>
2021-09-14 11:59:04 +0800

    add helpers for workspacelib

--- src/Utils_WS.cpp
+++ src/Utils_WS.cpp
@@ -1,1 +1,36 @@
 #include "Utils_WS.h"
+
+#include "TMath.h"
+
+#include <Math/DistFunc.h>
+#include <Math/QuantFuncMathCore.h>
+
+namespace Utils_WS
+{
+
+double pvalueToSignificance(double p)
+{
+    // return TMath::Sqrt(2.0) * TMath::ErfInverse(1.0 - 2.0 * p);
+    return TMath::NormQuantile(1.0 - p);
+}
+
+double significanceToPvalue(double z)
+{
+    // return 0.5 * (1.0 - TMath::Erf(z / TMath::Sqrt(2.0)));
+    return 1.0 - ROOT::Math::gaussian_cdf(z);
+}
+
+double q0ToSignificance(double q0)
+{
+    return TMath::Sqrt(q0);
+}
+
+double nllToQ0(double nllMu0, double nllMuHat, double muHat)
+{
+    double q0 = (muHat < 0) ? 0 : (2 * (nllMu0 - nllMuHat));
+    if (q0 < 0.) q0 = 0;
+    return q0;
+}
+
+
+}
--- src/Utils_WS.h
+++ src/Utils_WS.h
@@ -4,6 +4,38 @@
 namespace Utils_WS
 {
     constexpr float lumiRelError = 0.017;
+
+    /**
+     * @brief Calculate significance given p-value
+     *
+     *             -1
+     * Z = √2 x erf  (1 - 2p)
+     *
+     */
+    double pvalueToSignificance(double p);
+
+    /**
+     * @brief Calculate p-value given significance
+     *
+     *     1               Z
+     * p = - x [ 1 - erf (---)]
+     *     2              √2
+     *
+     */
+    double significanceToPvalue(double z);
+
+    /**
+     * @todo Check the definition
+     *
+     */
+    double q0ToSignificance(double q0);
+
+    /**
+     * @todo Check the definition
+     *
+     */
+    double nllToQ0(double nllMu0, double nllMuHat, double muHat);
+
 }
 
 #endif
