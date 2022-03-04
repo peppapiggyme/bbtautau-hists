ce6a06a74508ce363d64d958834beac4429af4ea
Bowen Zhang <bowen.zhang@cern.ch>
2022-03-04 21:44:40 +0800

    pseudo data

--- examples/Example_PseudoData.cpp
+++ examples/Example_PseudoData.cpp
@@ -5,7 +5,7 @@
 #include <regex>
 
 static void test_pseudodata_helper(const std::string& filename,
-    double& obs_q0, double& obs_sig, int& status_con, int& status_uncon, int& sign);
+    double& obs_q0, double& obs_sig, double& mu_hat_uncon, int& status_con, int& status_uncon, int& sign);
 
 void test_pseudodata(const std::vector<std::string>& filenames, const std::string& outfile)
 {
@@ -16,6 +16,7 @@ void test_pseudodata(const std::vector<std::string>& filenames, const std::strin
     double obs_q0 = -1;
     double obs_sig = 0;
     double obs_pvalue = 1;
+    double mu_hat_uncon = 0;
     int status_con = -1;
     int status_uncon = -1;
     int sign = 0;
@@ -26,6 +27,7 @@ void test_pseudodata(const std::vector<std::string>& filenames, const std::strin
     t->Branch("obs_pvalue", &obs_pvalue, "obs_pvalue/D");
     t->Branch("status_con", &status_con, "status_con/I");
     t->Branch("status_uncon", &status_uncon, "status_uncon/I");
+    t->Branch("mu_hat_uncon", &mu_hat_uncon, "mu_hat_uncon/D");
     t->Branch("sign", &sign, "sign/I");
     t->Branch("pseudo_data_idx", &pseudo_data_idx, "pseudo_data_idx/I");
     
@@ -42,7 +44,7 @@ void test_pseudodata(const std::vector<std::string>& filenames, const std::strin
             throw std::runtime_error("no match!");
         }
 
-        test_pseudodata_helper(filename, obs_q0, obs_sig, status_con, status_uncon, sign);
+        test_pseudodata_helper(filename, obs_q0, obs_sig, mu_hat_uncon, status_con, status_uncon, sign);
         obs_pvalue = Utils_WS::significanceToPvalue(obs_sig);
         t->Fill();
     }
@@ -56,7 +58,7 @@ void test_pseudodata(const std::vector<std::string>& filenames, const std::strin
 }
 
 void test_pseudodata_helper(const std::string& filename, 
-    double& obs_q0, double& obs_sig, int& status_con, int& status_uncon, int& sign)
+    double& obs_q0, double& obs_sig, double& mu_hat_uncon, int& status_con, int& status_uncon, int& sign)
 {
     const int doUncap = 1;
 
@@ -73,7 +75,7 @@ void test_pseudodata_helper(const std::string& filename,
     
     WorkSpace* wst = new WorkSpace(info);
 
-    wst->GetRooWorkspace()->loadSnapshot("conditionalNuis_0");
+    // wst->GetRooWorkspace()->loadSnapshot("snapshot_paramsVals_initial");
 
     ((RooRealVar*)wst->GetRooPOIs()->first())->setVal(0);
     ((RooRealVar*)wst->GetRooPOIs()->first())->setConstant(1);
@@ -82,9 +84,11 @@ void test_pseudodata_helper(const std::string& filename,
 
     double obs_nll_cond = ((RooRealVar*)wst->GetRooNLL())->getVal();
 
+    // wst->GetRooWorkspace()->loadSnapshot("snapshot_paramsVals_initial");
     ((RooRealVar*)wst->GetRooPOIs()->first())->setConstant(0);
     wst->FitAll();
     status_uncon = wst->GetMinimizationStatus();
+    mu_hat_uncon = ((RooRealVar*)wst->GetRooPOIs()->first())->getVal();
 
     double obs_nll_min = ((RooRealVar*)wst->GetRooNLL())->getVal();
 
--- jobs/submit_pd.sh
+++ jobs/submit_pd.sh
@@ -1,59 +1,37 @@
 #!/bin/bash
 
+TARFILE=$1
+LIST_OF_INPUT=$2
+OUTPUT_ROOTFILE=$3
+
 INPUT_PATH=/publicfs/atlas/atlasnew/higgs/hh2X/zhangbw/bootstrap_output/
-OUTPUT_PATH=/publicfs/atlas/atlasnew/higgs/hh2X/zhangbw/ntuple/bootstrap_ntuple/
-TRUNK_IDX=$1
-MASS=$2
+echo "======================="
+echo "UNTAR WITH ${LIST_OF_INPUT}"
+echo "======================="
 
-pushd $INPUT_PATH
-
-tar -zxf trunk-$TRUNK_IDX-$MASS.tar.gz
-
-mkdir -p /tmp/bowenzhang/
-fileTmpListPrefix=/tmp/bowenzhang/ListOfPseudoData-${TRUNK_IDX}
-
-rm -f $fileTmpListPrefix-$MASS.txt
-touch $fileTmpListPrefix-$MASS.txt
-list=$( tar -tf trunk-${TRUNK_IDX}-$MASS.tar.gz )
-for elem in $list
-do 
-    echo "$INPUT_PATH/$elem" >> $fileTmpListPrefix-$MASS.txt
-done
-
-split $fileTmpListPrefix-$MASS.txt $fileTmpListPrefix-$MASS.txt. -d -l 100
-
-ls /tmp/bowenzhang/
-
+pushd ${INPUT_PATH}
+tar -zxf  "${TARFILE}"  $( cat ${LIST_OF_INPUT} | egrep -o "[0-9]+\.[0-9]+\.root" )
 popd
 
-BASE_DIR=${PWD}
+BASE_DIR=/scratchfs/atlas/bowenzhang/bbtautau-hists/
 cd ${BASE_DIR} && source ${BASE_DIR}/setup.sh
 
-cd ${BASE_DIR}/../build-bbtautau-hists/
+echo "======================="
+echo "EXECUTING PART ${PART}"
+echo "======================="
 
+./run-pd ${LIST_OF_INPUT} ${OUTPUT_ROOTFILE}
 
-for PART in 00 01 02 03 04 05 06 07 08 09
+echo "======================="
+echo "REMOVING TMP WORKSPACES"
+echo "======================="
+
+while read -r line
 do
-    echo "======================="
-    echo "EXECUTING PART ${PART}"
-    echo "======================="
-
-    ./run-pd $fileTmpListPrefix-$MASS.txt.${PART} $OUTPUT_PATH/params_${TRUNK_IDX}_${MASS}.${PART}.root
-
-    echo "======================="
-    echo "REMOVING TMP WORKSPACES"
-    echo "======================="
-
-    while read -r line
-    do
-        rm -f $line
-    done < $fileTmpListPrefix-$MASS.txt.${PART}
-
-    echo "======================="
-    echo "FINISHING PART ${PART}"
-    echo "======================="
-done
+    rm -f $line
+done < ${LIST_OF_INPUT}
 
 echo "======================="
 echo "Finito !"
+echo ${OUTPUT_ROOTFILE}
 echo "======================="
